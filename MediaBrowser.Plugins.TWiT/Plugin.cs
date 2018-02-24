c390d330dc4fd4c14f7aa665f2ea20206a256e87
Luke Pulverenti <luke.pulverenti@gmail.com>
2018-02-23 23:14:32 -0500

    compatibility update

---
+++ MediaBrowser.Plugins.TWiT/Plugin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using MediaBrowser.Plugins.TWiT.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace MediaBrowser.Plugins.TWiT
+{
+    /// <summary>
+    /// Class Plugin
+    /// </summary>
+    public class Plugin : BasePlugin<PluginConfiguration>
+    {
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        /// <summary>
+        /// Gets the name of the plugin
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get { return "TWiT"; }
+        }
+
+        private Guid _id = new Guid("069B9C9C-C149-48B0-B950-2671EFCD8043");
+        public override Guid Id
+        {
+            get { return _id; }
+        }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public override string Description
+        {
+            get
+            {
+                return "Watch your favorite TWiT.tv webcasts.";
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <value>The instance.</value>
+        public static Plugin Instance { get; private set; }
+    }
+}
---
+++ MediaBrowser.Plugins.TWiT/TwitChannelItemsDownloader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediaBrowser.Common.Net;
+using MediaBrowser.Model.Logging;
+using MediaBrowser.Model.Serialization;
+using System.IO;
+using System.Text;
+
+namespace MediaBrowser.Plugins.TWiT
+{
+    public class TwitChannelItemsDownloader
+    {
+        private ILogger _logger;
+        private readonly IHttpClient _httpClient;
+        private readonly IXmlSerializer _xmlSerializer;
+
+        public TwitChannelItemsDownloader(ILogger logManager, IXmlSerializer xmlSerializer, IHttpClient httpClient)
+        {
+            _logger = logManager;
+            _xmlSerializer = xmlSerializer;
+            _httpClient = httpClient;
+        }
+
+        public async Task<rss> GetStreamList(String queryUrl, int offset, CancellationToken cancellationToken)
+        {
+            using (var response = await _httpClient.GetResponse(new HttpRequestOptions
+            {
+
+                CancellationToken = cancellationToken,
+                Url = queryUrl,
+                EnableDefaultUserAgent = true,
+                EnableHttpCompression = false,
+                BufferContent = false,
+                EnableKeepAlive = false
+
+            }).ConfigureAwait(false))
+            {
+                using (var xml = response.Content)
+                {
+                    _logger.Info("Reading TWiT response with StreamReader");
+
+                    using (var reader = new StreamReader(xml))
+                    {
+                        var str = reader.ReadToEnd();
+
+                        _logger.Info("Deserializing TwiT response");
+
+                        rss result = _xmlSerializer.DeserializeFromBytes(typeof(rss), Encoding.UTF8.GetBytes(str)) as rss;
+                        _logger.Info("Deserialized TwiT response");
+                        return result;
+                    }
+                }
+            }
+        }
+
+    }
+}
